6919757d1838ff4a6201864717a65e702cfeadb3
plamen <pgnikolov@gmail.com>
2024-07-21 22:24:49 +0200

    js tasks

---
+++ easter-lunch.js
@@ -0,0 +1,13 @@
+function easterLunch(input){
+    const easterBreadPrice = 3.20
+    const eggsPrice = 4.35
+    const cookiesPrice = 5.40
+    const eggPaint = 0.15
+
+    let easterBreadCount = parseInt(input[0])
+    let eggDozens = parseInt(input[1])
+    let cookeisWeight = parseInt(input[2])
+
+    let totalCost = easterBreadCount * easterBreadPrice + eggDozens * eggsPrice + eggDozens * 12 * eggPaint + cookeisWeight * cookiesPrice
+    console.log(`${totalCost.toFixed(2)}`)
+}
